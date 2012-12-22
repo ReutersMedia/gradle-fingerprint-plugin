a42dd9e50340eb3c30ef30e051d499839d1402b9
Tal Giat <talgiat@gmail.com>
2012-12-22 04:13:35 -0500

    adding license and test resources, fixing many bugs

--- src/main/groovy/com/reuters/gradle/FingerprintTask.groovy
+++ src/main/groovy/com/reuters/gradle/FingerprintTask.groovy
@@ -1,5 +1,6 @@
 package com.reuters.gradle
 
+import org.gradle.api.file.FileTree
 import org.gradle.api.internal.tasks.execution.TaskValidator
 import org.gradle.api.tasks.SourceTask
 import org.gradle.api.tasks.TaskAction
@@ -8,17 +9,20 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Optional
 
 class FingerprintTask extends SourceTask {
+
+    public static final String FINGERPRINT_LENGHT_OUTSIDE_RANAGE_MESSAGE =
+        'fingerprintLength should be between 8 and 20'
+
     @OutputDirectory def destinationDir
-
     @Input @Optional def fingerprintLength
-
-    @Input def replaceNameInFiles
+    @Input FileTree replaceInFiles
+    @Input @Optional def replacedDestDir
 
     { // add validator for length
         addValidator({ task, messages ->
             if (fingerprintLength != null &&
                 (!fingerprintLength instanceof Integer || fingerprintLength < 8 || fingerprintLength > 20)) {
-                messages.add('fingerprintLength should be between 8 and 20')
+                messages.add(FINGERPRINT_LENGHT_OUTSIDE_RANAGE_MESSAGE)
             }
         } as TaskValidator)
     }
@@ -27,17 +31,47 @@ class FingerprintTask extends SourceTask {
         project.file(destinationDir)
     }
 
+    File getReplacedDestDir() {
+        replacedDestDir != null ? project.file(replacedDestDir) : destinationDir
+    }
+
     @TaskAction
     def doFingerprint() {
-        source.visit { element ->
-            if (!element.directory) {
-                project.ant.checksum(file: element.file, algorithm: 'SHA', property: 'checksum')
-                def checksum = fingerprintLength ? project.ant.properties['checksum'][1..fingerprintLength] :
-                    project.ant.properties['checksum']
-                def outputFile = buildOutputFilename(element.file, checksum)
-                def relativePath = element.relativePath.replaceLastName('')
-                project.ant.copyfile(src: element.file.absolutePath,
+        // sort files by path+name length so we first match the longest ones
+        def filenameMap = new TreeMap([ compare: {a, b ->
+            if (b.length() - a.length() == 0) {
+                b.compareTo(a)
+            } else {
+                b.length() - a.length()
+            }
+            }] as Comparator)
+        source.visit { sourceFile ->
+            if (!sourceFile.directory) {
+                def checksumProperty = "$sourceFile.file.absolutePath-checksum"
+                project.ant.checksum(file: sourceFile.file, algorithm: 'SHA', property: checksumProperty)
+                def checksum = fingerprintLength ? project.ant.properties[checksumProperty][1..fingerprintLength] :
+                    project.ant.properties[checksumProperty]
+                def outputFile = buildOutputFilename(sourceFile.file, checksum)
+                def relativePath = sourceFile.relativePath.replaceLastName('')
+                project.ant.copyfile(src: sourceFile.file.absolutePath,
                     dest: "$destinationDir.path/$relativePath$outputFile")
+                filenameMap["$relativePath$sourceFile.file.name"] = "$relativePath$outputFile"
+            }
+        }
+        println filenameMap
+        // only try to replace names if we actually fingerprinted some files
+        if (filenameMap.size() > 0) {
+            replaceInFiles.visit { replaceFile ->
+                if (!replaceFile.directory) {
+                    def contents = replaceFile.file.text
+                    filenameMap.each() { originalName, fingerprintedName ->
+                        contents = contents.replaceAll(originalName, fingerprintedName)
+                    }
+                    def relativePath = replaceFile.relativePath.replaceLastName('')
+                    new File("$replacedDestDir.path/$relativePath").mkdirs()
+                    def file = new File("$replacedDestDir.path/$relativePath$replaceFile.file.name")
+                    file.write(contents)
+                }
             }
         }
     }
--- src/test/groovy/com/reuters/gradle/FingerprintTaskTest.groovy
+++ src/test/groovy/com/reuters/gradle/FingerprintTaskTest.groovy
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertTrue
 
 import org.gradle.testfixtures.ProjectBuilder
 import org.gradle.api.Project
+import org.gradle.api.file.FileTree
 import org.junit.Test
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
@@ -17,27 +18,47 @@ class FingerprintTaskTest {
     @Test
     public void canAddTaskToProject() {
         def task = project.task('fingerprint', type: FingerprintTask)
-        List<String> messages = []
         assertTrue(task instanceof FingerprintTask)
     }
 
     @Test
+    public void fingerprintLenghNotInRange() {
+        def (resorucesDir, task) = createAndConfigureTask('**/*.txt', '**/*.html', 4)
+        List<String> messages = []
+        task.validators*.validate(task,messages)
+        assertTrue(messages.size == 1)
+        messages.contains(FingerprintTask.FINGERPRINT_LENGHT_OUTSIDE_RANAGE_MESSAGE)
+    }
+
+    @Test
     public void generatesChecksum() {
-        def srcDir = tempDir.newFolder()
-        addTestFile(srcDir, 'test.js');
-        def task = project.task('fingerprint', type: FingerprintTask)
-        task.source = srcDir
-        task.destinationDir = srcDir
-        task.fingerprintLength = 10
+        def (resorucesDir, task) = createAndConfigureTask('**/*.png', '**/*.css', 10)
         task.doFingerprint()
-        verifyChecksumFile(srcDir.absolutePath, task.destinationDir.absolutePath, 'test.js', 10)
+        verifyChecksumFiles('**/*.png', 10)
+    }
+
+    def createAndConfigureTask(sourceInclude, replaceFilesInclude, fingerprintLength) {
+        def task = project.task('fingerprint', type: FingerprintTask)
+        def cssResource = this.getClass().getResource('/css').toString() - 'file:'
+        def resourcesDir = cssResource - '/css'
+        def buildDir = resourcesDir - '/resources/test'
+        task.source = project.fileTree(dir: resourcesDir, include: sourceInclude)
+        task.destinationDir = project.file(buildDir)
+        if (fingerprintLength > 0 ) {
+            task.fingerprintLength = fingerprintLength
+        }
+        task.replaceInFiles = project.fileTree(dir: resourcesDir , include: replaceFilesInclude)
+        return [resourcesDir, task]
+    }
+
+    def verifyChecksumFiles(includePattren, fingerprintLength) {
+
     }
 
     def verifyChecksumFile(srcDir, destDir, filename, fingerprintLength) {
         def fileExtension = filename.lastIndexOf('.').with { index -> index > -1 ? filename[index..-1] : ''}
         def filenameOnly = filename - fileExtension
         def fileFilter = "$filenameOnly-.{$fingerprintLength}\\$fileExtension"
-        println fileFilter
         def destFile = []
         new File(destDir).eachFileMatch ~fileFilter, { file ->
             destFile << file.name
@@ -58,15 +79,5 @@ class FingerprintTaskTest {
             }
         }
         return true
-    }
-
-    def addTestFile(dir, filename) {
-        new File("$dir").mkdirs()
-        addFile("$dir/$filename", "some dummy content")
-    }
-
-    def addFile(name, contents) {
-        def file = new File(name)
-        file << contents
     }
 }
